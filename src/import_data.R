6c0784879c4a54e4bce0156e6cfc21e55d7379a1
Fortunat <fortunat1993@gmail.com>
2017-05-26 10:57:55 +0200

    add regularity and transilien duration details

--- src/import_data.R
+++ src/import_data.R
@@ -34,6 +34,78 @@ transilien_ligne_par_gare <- read.csv(
   stringsAsFactors=FALSE
 )
 
+transilien_regularite_intercite <- read.csv(
+  file=paste0(getwd(),"/data/regularite-mensuelle-intercites.csv"), 
+  header = TRUE,
+  sep = ";",
+  encoding="UTF-8",
+  stringsAsFactors=FALSE
+)
+
+transilien_regularite_transilien <- read.csv(
+  file=paste0(getwd(),"/data/ponctualite-mensuelle-transilien.csv"), 
+  header = TRUE,
+  sep = ";",
+  encoding="UTF-8",
+  stringsAsFactors=FALSE
+)
+
+transilien_regularite_tgv <- read.csv(
+  file=paste0(getwd(),"/data/regularite-mensuelle-tgv.csv"), 
+  header = TRUE,
+  sep = ";",
+  encoding="UTF-8",
+  stringsAsFactors=FALSE
+)
+
+transilien_regularite_ter <- read.csv(
+  file=paste0(getwd(),"/data/regularite-mensuelle-ter.csv"), 
+  header = TRUE,
+  sep = ";",
+  encoding="UTF-8",
+  stringsAsFactors=FALSE
+)
+
+transilien_trips <- read.csv(
+  file=paste0(getwd(),"/data/transilien_gtfs/trips.txt"), 
+  header = TRUE,
+  sep = ",",
+  encoding="UTF-8",
+  stringsAsFactors=FALSE
+)
+
+transilien_stops <- read.csv(
+  file=paste0(getwd(),"/data/transilien_gtfs/stops.txt"), 
+  header = TRUE,
+  sep = ",",
+  encoding="UTF-8",
+  stringsAsFactors=FALSE
+)
+
+transilien_stop_times <- read.csv(
+  file=paste0(getwd(),"/data/transilien_gtfs/stop_times.txt"), 
+  header = TRUE,
+  sep = ",",
+  encoding="UTF-8",
+  stringsAsFactors=FALSE
+)
+
+transilien_calendar <- read.csv(
+  file=paste0(getwd(),"/data/transilien_gtfs/calendar.txt"), 
+  header = TRUE,
+  sep = ",",
+  encoding="UTF-8",
+  stringsAsFactors=FALSE
+)
+
+transilien_routes <- read.csv(
+  file=paste0(getwd(),"/data/transilien_gtfs/routes.txt"), 
+  header = TRUE,
+  sep = ",",
+  encoding="UTF-8",
+  stringsAsFactors=FALSE
+)
+
 ####################################################
 ################ !GET TRANSILIEN TRAFIC ############
 ####################################################
@@ -42,6 +114,78 @@ transilien_ligne_par_gare <- read.csv(
 #########################################
 ################ CLEAN DATA #############
 #########################################
+
+lapply(names(transilien_routes), function(item)
+{
+  transilien_routes[[item]][
+    is.na(transilien_routes[[item]]) |
+      transilien_routes[[item]] == ""
+    ] <<- "Undefined"
+})
+
+lapply(names(transilien_calendar), function(item)
+{
+  transilien_calendar[[item]][
+    is.na(transilien_calendar[[item]]) |
+      transilien_calendar[[item]] == ""
+    ] <<- "Undefined"
+})
+
+lapply(names(transilien_stop_times), function(item)
+{
+  transilien_stop_times[[item]][
+    is.na(transilien_stop_times[[item]]) |
+      transilien_stop_times[[item]] == ""
+    ] <<- "Undefined"
+})
+
+lapply(names(transilien_stops), function(item)
+{
+  transilien_stops[[item]][
+    is.na(transilien_stops[[item]]) |
+      transilien_stops[[item]] == ""
+    ] <<- "Undefined"
+})
+
+lapply(names(transilien_trips), function(item)
+{
+  transilien_trips[[item]][
+    is.na(transilien_trips[[item]]) |
+      transilien_trips[[item]] == ""
+    ] <<- "Undefined"
+})
+
+lapply(names(transilien_regularite_ter), function(item)
+{
+  transilien_regularite_ter[[item]][
+    is.na(transilien_regularite_ter[[item]]) |
+      transilien_regularite_ter[[item]] == ""
+    ] <<- "Undefined"
+})
+
+lapply(names(transilien_regularite_tgv), function(item)
+{
+  transilien_regularite_tgv[[item]][
+    is.na(transilien_regularite_tgv[[item]]) |
+      transilien_regularite_tgv[[item]] == ""
+    ] <<- "Undefined"
+})
+
+lapply(names(transilien_regularite_transilien), function(item)
+{
+  transilien_regularite_transilien[[item]][
+    is.na(transilien_regularite_transilien[[item]]) |
+      transilien_regularite_transilien[[item]] == ""
+    ] <<- "Undefined"
+})
+
+lapply(names(transilien_regularite_intercite), function(item)
+{
+  transilien_regularite_intercite[[item]][
+    is.na(transilien_regularite_intercite[[item]]) |
+      transilien_regularite_intercite[[item]] == ""
+    ] <<- "Undefined"
+})
 
 lapply(names(transilien_ligne_par_gare), function(item)
 {
@@ -91,10 +235,28 @@ save(
   transilien_incident_securite,
   transilien_localisation_gare,
   transilien_ligne_par_gare,
+  transilien_regularite_intercite,
+  transilien_regularite_transilien,
+  transilien_regularite_tgv,
+  transilien_regularite_ter,
+  transilien_trips,
+  transilien_stops,
+  transilien_stop_times,
+  transilien_calendar,
+  transilien_routes,
   df_transilien_ligne_par_gare,
   df_transilien_localisation_gare,
   df_transilien_incident_securite,
-  df_transilien_trafic
+  df_transilien_trafic,
+  df_transilien_regularite_intercite,
+  df_transilien_regularite_transilien,
+  df_transilien_regularite_tgv,
+  df_transilien_regularite_ter,
+  df_transilien_trips,
+  df_transilien_stops,
+  df_transilien_stop_times,
+  df_transilien_calendar,
+  df_transilien_routes
 )
 
 #########################################
--- src/initialize_spark.R
+++ src/initialize_spark.R
@@ -14,6 +14,15 @@ df_transilien_ligne_par_gare <- as.DataFrame(transilien_ligne_par_gare)
 df_transilien_localisation_gare <- as.DataFrame(transilien_localisation_gare)
 df_transilien_incident_securite <- as.DataFrame(transilien_incident_securite)
 df_transilien_trafic <- as.DataFrame(transilien_trafic)
+df_transilien_regularite_intercite <- as.DataFrame(transilien_regularite_intercite)
+df_transilien_regularite_transilien <- as.DataFrame(transilien_regularite_transilien)
+df_transilien_regularite_tgv <- as.DataFrame(transilien_regularite_tgv)
+df_transilien_regularite_ter <- as.DataFrame(transilien_regularite_ter)
+df_transilien_trips <- as.DataFrame(transilien_trips)
+df_transilien_stops <- as.DataFrame(transilien_stops)
+df_transilien_stop_times <- as.DataFrame(transilien_stop_times)
+df_transilien_calendar <- as.DataFrame(transilien_calendar)
+df_transilien_routes <- as.DataFrame(transilien_routes)
 #!CONVERT R DATAFRAME OBJET INTO SPARK OBJECT
 
 #STOP SPARK SESSION
