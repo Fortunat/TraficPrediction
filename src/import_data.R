c3dd6bd9a0d6c8a750c3b1ec71a239a2027ab5d8
Fortunat <fortunat1993@gmail.com>
2017-05-27 23:05:04 +0200

    generate first RandomForrest predictive model

--- src/import_data.R
+++ src/import_data.R
@@ -10,98 +10,10 @@ transilien_trafic <- read.csv(
   stringsAsFactors=FALSE
 )
 
-transilien_incident_securite <- read.csv(
-  file=paste0(getwd(),"/data/incidents-securite.csv"), 
-  header = TRUE,
-  sep = ";",
-  encoding="UTF-8",
-  stringsAsFactors=FALSE
-)
-
-transilien_localisation_gare <- read.csv(
-  file=paste0(getwd(),"/data/sncf-gares-et-arrets-transilien-ile-de-france.csv"), 
-  header = TRUE,
-  sep = ";",
-  encoding="UTF-8",
-  stringsAsFactors=FALSE
-)
-
-transilien_ligne_par_gare <- read.csv(
-  file=paste0(getwd(),"/data/sncf-lignes-par-gares-idf.csv"), 
-  header = TRUE,
-  sep = ";",
-  encoding="UTF-8",
-  stringsAsFactors=FALSE
-)
-
-transilien_regularite_intercite <- read.csv(
-  file=paste0(getwd(),"/data/regularite-mensuelle-intercites.csv"), 
-  header = TRUE,
-  sep = ";",
-  encoding="UTF-8",
-  stringsAsFactors=FALSE
-)
-
 transilien_regularite_transilien <- read.csv(
   file=paste0(getwd(),"/data/ponctualite-mensuelle-transilien.csv"), 
   header = TRUE,
   sep = ";",
-  encoding="UTF-8",
-  stringsAsFactors=FALSE
-)
-
-transilien_regularite_tgv <- read.csv(
-  file=paste0(getwd(),"/data/regularite-mensuelle-tgv.csv"), 
-  header = TRUE,
-  sep = ";",
-  encoding="UTF-8",
-  stringsAsFactors=FALSE
-)
-
-transilien_regularite_ter <- read.csv(
-  file=paste0(getwd(),"/data/regularite-mensuelle-ter.csv"), 
-  header = TRUE,
-  sep = ";",
-  encoding="UTF-8",
-  stringsAsFactors=FALSE
-)
-
-transilien_trips <- read.csv(
-  file=paste0(getwd(),"/data/transilien_gtfs/trips.txt"), 
-  header = TRUE,
-  sep = ",",
-  encoding="UTF-8",
-  stringsAsFactors=FALSE
-)
-
-transilien_stops <- read.csv(
-  file=paste0(getwd(),"/data/transilien_gtfs/stops.txt"), 
-  header = TRUE,
-  sep = ",",
-  encoding="UTF-8",
-  stringsAsFactors=FALSE
-)
-
-transilien_stop_times <- read.csv(
-  file=paste0(getwd(),"/data/transilien_gtfs/stop_times.txt"), 
-  header = TRUE,
-  sep = ",",
-  encoding="UTF-8",
-  stringsAsFactors=FALSE
-)
-
-transilien_calendar <- read.csv(
-  file=paste0(getwd(),"/data/transilien_gtfs/calendar.txt"), 
-  header = TRUE,
-  sep = ",",
-  encoding="UTF-8",
-  stringsAsFactors=FALSE
-)
-
-transilien_routes <- read.csv(
-  file=paste0(getwd(),"/data/transilien_gtfs/routes.txt"), 
-  header = TRUE,
-  sep = ",",
   encoding="UTF-8",
   stringsAsFactors=FALSE
 )
@@ -115,108 +27,12 @@ transilien_routes <- read.csv(
 ################ CLEAN DATA #############
 #########################################
 
-lapply(names(transilien_routes), function(item)
-{
-  transilien_routes[[item]][
-    is.na(transilien_routes[[item]]) |
-      transilien_routes[[item]] == ""
-    ] <<- "Undefined"
-})
-
-lapply(names(transilien_calendar), function(item)
-{
-  transilien_calendar[[item]][
-    is.na(transilien_calendar[[item]]) |
-      transilien_calendar[[item]] == ""
-    ] <<- "Undefined"
-})
-
-lapply(names(transilien_stop_times), function(item)
-{
-  transilien_stop_times[[item]][
-    is.na(transilien_stop_times[[item]]) |
-      transilien_stop_times[[item]] == ""
-    ] <<- "Undefined"
-})
-
-lapply(names(transilien_stops), function(item)
-{
-  transilien_stops[[item]][
-    is.na(transilien_stops[[item]]) |
-      transilien_stops[[item]] == ""
-    ] <<- "Undefined"
-})
-
-lapply(names(transilien_trips), function(item)
-{
-  transilien_trips[[item]][
-    is.na(transilien_trips[[item]]) |
-      transilien_trips[[item]] == ""
-    ] <<- "Undefined"
-})
-
-lapply(names(transilien_regularite_ter), function(item)
-{
-  transilien_regularite_ter[[item]][
-    is.na(transilien_regularite_ter[[item]]) |
-      transilien_regularite_ter[[item]] == ""
-    ] <<- "Undefined"
-})
-
-lapply(names(transilien_regularite_tgv), function(item)
-{
-  transilien_regularite_tgv[[item]][
-    is.na(transilien_regularite_tgv[[item]]) |
-      transilien_regularite_tgv[[item]] == ""
-    ] <<- "Undefined"
-})
-
 lapply(names(transilien_regularite_transilien), function(item)
 {
   transilien_regularite_transilien[[item]][
     is.na(transilien_regularite_transilien[[item]]) |
       transilien_regularite_transilien[[item]] == ""
     ] <<- "Undefined"
-})
-
-lapply(names(transilien_regularite_intercite), function(item)
-{
-  transilien_regularite_intercite[[item]][
-    is.na(transilien_regularite_intercite[[item]]) |
-      transilien_regularite_intercite[[item]] == ""
-    ] <<- "Undefined"
-})
-
-lapply(names(transilien_ligne_par_gare), function(item)
-{
-  transilien_ligne_par_gare[[item]][
-    is.na(transilien_ligne_par_gare[[item]]) |
-    transilien_ligne_par_gare[[item]] == ""
-  ] <<- "0"
-})
-
-lapply(names(transilien_localisation_gare), function(item)
-{
-  transilien_localisation_gare[[item]][
-    is.na(transilien_localisation_gare[[item]]) |
-    transilien_localisation_gare[[item]] == ""
-  ] <<- "Undefined"
-})
-
-lapply(names(transilien_incident_securite), function(item)
-{
-  transilien_incident_securite[[item]][
-    is.na(transilien_incident_securite[[item]]) |
-    transilien_incident_securite[[item]] == ""
-  ] <<- "Undefined"
-})
-
-lapply(names(transilien_trafic), function(item)
-{
-  transilien_trafic[[item]][
-    is.na(transilien_trafic[[item]]) |
-    transilien_trafic[[item]] == ""
-  ] <<- "Undefined"
 })
 
 #########################################
@@ -230,7 +46,7 @@ lapply(names(transilien_trafic), function(item)
 
 # transilien_regularite_transilien et transilien_trafic UTILE POUR LA PREDICTION
 
-#ADD FEATURE FOR TRANSILIEN_TRAFIC
+# ADD FEATURE FOR TRANSILIEN_TRAFIC
 transilien_trafic$Date.de.comptage <- as.Date( transilien_trafic$Date.de.comptage, '%Y-%m-%d')
 transilien_trafic$Date <- format(transilien_trafic$Date.de.comptage, '%Y-%m')
 
@@ -241,6 +57,73 @@ merged_transilien_trafic <- merge(
   by = c("Ligne", "Date"),
   all.y = TRUE
 )
+
+#LIMIT PERIMETER
+merged_transilien_trafic <- merged_transilien_trafic[merged_transilien_trafic$Ligne != "C",]
+
+#FORMAT
+merged_transilien_trafic$Montants <- as.numeric(merged_transilien_trafic$Montants)
+merged_transilien_trafic$Taux.de.ponctualité <- as.double(merged_transilien_trafic$Taux.de.ponctualité)
+merged_transilien_trafic$Code.Gare <- as.numeric(merged_transilien_trafic$Code.Gare)
+merged_transilien_trafic$Date.de.comptage <- as.Date( merged_transilien_trafic$Date.de.comptage, '%Y-%m-%d')
+#!FORMAT
+
+#NUMBER OF ROW
+merged_transilien_trafic_count <- nrow(merged_transilien_trafic)
+
+#GIVE RANDOM VALUE FOR EACH
+merged_transilien_trafic$alea <- runif(merged_transilien_trafic_count, min=0, max=1)
+
+merged_transilien_trafic$Intensite <- "Undefined"
+
+#FAIBLE INTENSITE USER
+merged_transilien_trafic[
+  merged_transilien_trafic$Montants >= 0 
+  & merged_transilien_trafic$Montants <= 1000
+,]$Intensite <- "faible"
+
+#MOYENNE INTENSITE USER
+merged_transilien_trafic[
+  merged_transilien_trafic$Montants > 1000 
+  & merged_transilien_trafic$Montants <= 2000
+,]$Intensite <- "moyenne"
+
+#FORTE INTENSITE USER
+merged_transilien_trafic[
+  merged_transilien_trafic$Montants > 2000 
+,]$Intensite <- "forte"
+
+lapply(names(merged_transilien_trafic), function(item)
+{
+  merged_transilien_trafic[[item]][
+    is.na(merged_transilien_trafic[[item]]) |
+      merged_transilien_trafic[[item]] == ""
+    ] <<- "Undefined"
+})
+
+#TRAIN DATA
+merged_transilien_trafic.train <- merged_transilien_trafic[
+  merged_transilien_trafic$alea <= 0.667,
+  c(
+    "Nom.gare",
+    "Type.jour",
+    "Date.de.comptage",
+    "Tranche.horaire",
+    "Intensite"
+  )
+]
+
+#TEST DATA
+merged_transilien_trafic.test <- merged_transilien_trafic[
+  merged_transilien_trafic$alea > 0.667,
+  c(
+    "Nom.gare",
+    "Type.jour",
+    "Date.de.comptage",
+    "Tranche.horaire",
+    "Intensite"
+  )
+]
 
 ############################################
 ################ !PREPARE DATA #############
@@ -258,31 +141,7 @@ source(paste0(getwd(), "/src/initialize_spark.R"))
 save(
   file=paste0(getwd(), "/.RData"),
   transilien_trafic,
-  transilien_incident_securite,
-  transilien_localisation_gare,
-  transilien_ligne_par_gare,
-  transilien_regularite_intercite,
-  transilien_regularite_transilien,
-  transilien_regularite_tgv,
-  transilien_regularite_ter,
-  transilien_trips,
-  transilien_stops,
-  transilien_stop_times,
-  transilien_calendar,
-  transilien_routes,
-  df_transilien_ligne_par_gare,
-  df_transilien_localisation_gare,
-  df_transilien_incident_securite,
-  df_transilien_trafic,
-  df_transilien_regularite_intercite,
-  df_transilien_regularite_transilien,
-  df_transilien_regularite_tgv,
-  df_transilien_regularite_ter,
-  df_transilien_trips,
-  df_transilien_stops,
-  df_transilien_stop_times,
-  df_transilien_calendar,
-  df_transilien_routes
+  transilien_regularite_transilien
 )
 
 #########################################
--- src/initialize_spark.R
+++ src/initialize_spark.R
@@ -1,29 +1,29 @@
-Sys.setenv(SPARK_HOME = paste0(getwd(), "/spark-2.0.1-bin-hadoop2.7"))
-Sys.setenv('SPARKR_SUBMIT_ARGS'='"--packages" "com.databricks:spark-csv_2.10:1.4.0" "sparkr-shell"')
+# Sys.setenv(SPARK_HOME = paste0(getwd(), "/spark-2.0.1-bin-hadoop2.7"))
+Sys.setenv(SPARK_HOME = paste0(getwd(), "/spark-2.1.1-bin-hadoop2.7"))
 
 library(SparkR, lib.loc = c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib")))
 sparkR.session(
-  master = "local[*]", 
+  master = "local[*]",
   sparkConfig = list(spark.driver.memory = "1g"),
-  sparkPackages = "com.databricks:spark-csv_2.10:1.4.0",
+  # sparkPackages = "com.databricks:spark-csv_2.10:1.4.0",
+  sparkPackages = "com.databricks:spark-csv_2.11:1.5.0",
   enableHiveSupport = FALSE
 )
 
 #CONVERT R DATAFRAME OBJET INTO SPARK OBJECT
-df_transilien_ligne_par_gare <- as.DataFrame(transilien_ligne_par_gare)
-df_transilien_localisation_gare <- as.DataFrame(transilien_localisation_gare)
-df_transilien_incident_securite <- as.DataFrame(transilien_incident_securite)
-df_transilien_trafic <- as.DataFrame(transilien_trafic)
-df_transilien_regularite_intercite <- as.DataFrame(transilien_regularite_intercite)
-df_transilien_regularite_transilien <- as.DataFrame(transilien_regularite_transilien)
-df_transilien_regularite_tgv <- as.DataFrame(transilien_regularite_tgv)
-df_transilien_regularite_ter <- as.DataFrame(transilien_regularite_ter)
-df_transilien_trips <- as.DataFrame(transilien_trips)
-df_transilien_stops <- as.DataFrame(transilien_stops)
-df_transilien_stop_times <- as.DataFrame(transilien_stop_times)
-df_transilien_calendar <- as.DataFrame(transilien_calendar)
-df_transilien_routes <- as.DataFrame(transilien_routes)
+df_merged_transilien_trafic <- as.DataFrame(merged_transilien_trafic)
+df_merged_transilien_trafic.train <- as.DataFrame(merged_transilien_trafic.train)
+df_merged_transilien_trafic.test <- as.DataFrame(merged_transilien_trafic.test)
 #!CONVERT R DATAFRAME OBJET INTO SPARK OBJECT
+
+#MODEL
+model <- SparkR::spark.randomForest(df_merged_transilien_trafic.train, Intensite ~ ., type = "classification")
+#!MODEL
+
+#PREDICT
+prediction <- SparkR::predict(model, newData = df_merged_transilien_trafic.test)
+SparkR::showDF(SparkR::select(prediction,c('Intensite','Nom_gare', 'Type_jour', 'Tranche_horaire','rawPrediction','probability','prediction')),50)
+#!PREDICT
 
 #STOP SPARK SESSION
 sparkR.session.stop()
